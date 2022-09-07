5ed2dff28e326a0528907e4d8b26a2f55d421429
John <jdogan46@yahoo.com>
2022-09-07 10:38:49 -0500

    slight tweek that fixed an annotation connection

--- superherofinder/src/main/java/com/sg/superherofinder/SuperherofinderApplication.java
+++ superherofinder/src/main/java/com/sg/superherofinder/SuperherofinderApplication.java
@@ -2,8 +2,10 @@ package com.sg.superherofinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("module-service")
 public class SuperherofinderApplication {
 
 	public static void main(String[] args) {
